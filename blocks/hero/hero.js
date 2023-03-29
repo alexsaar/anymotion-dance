909157769b6af7af6a6bec53dfcbca5c7ac3d11a
Alexander Saar <alexander.saar@gmail.com>
2023-03-29 19:57:21 +0200

    handle scroll list navigation

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,11 +1,35 @@
+function setActiveLink() {
+  const banner = document.querySelectorAll('.hero > div');
+  [...banner].forEach((el) => {
+    const bounding = el.getBoundingClientRect();
+    if (bounding.top >= 0
+      && bounding.left >= 0
+      && bounding.right <= window.innerWidth
+      && bounding.bottom <= window.innerHeight) {
+      document.querySelector('.hero-slider-link.active').classList.remove('active');
+      document.querySelector(`[href="#${el.id}"]`).classList.add('active');
+    }
+  });
+}
+
 function onLinkClick(e) {
-  document.getElementById(e.srcElement.href.split('#')[1]).scrollIntoView(false);
   e.preventDefault();
+
+  const el = document.getElementById(e.srcElement.href.split('#')[1]);
+  el.scrollIntoView(false);
+
+  setActiveLink();
+}
+
+function onScroll(e) {
+  setTimeout(setActiveLink(e), 500);
 }
 
 export default function decorate(block) {
   const slider = document.createElement('div');
   slider.className = 'hero-slider';
+
+  block.onscroll = onScroll;
 
   if (block.children.length > 1) {
     [...block.children].forEach((el, index) => {
@@ -17,7 +41,7 @@ export default function decorate(block) {
       a.className = 'hero-slider-link';
       slider.append(a);
     });
-
+    slider.children[0].classList.add('active');
     block.parentElement.append(slider);
   }
 }
